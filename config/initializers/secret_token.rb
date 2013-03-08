87a7f448d73172f349a0a76bb26f0a37fc44788d
Chas Wareing <c3w@juicypop.net>
2013-03-07 16:11:58 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flourish::Application.config.secret_token = '296e9c8aa0a515c51a079a7fba56881e2980a96729476a0d95dfcc033aaeff0a807eba24438e33dad6a198a1983f79631914bc642833d7de9803a309e5f7cbaf'
